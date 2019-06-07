3cc3c02bd977b03be535ff2886633a773d6172db
harjothkhara <harjoth.khara@gmail.com>
2019-06-07 09:44:04 -0700

    helpers finished

--- data/helpers/actions-model.js
+++ data/helpers/actions-model.js
@@ -7,3 +7,45 @@ module.exports = {
     remove,
     update
 };
+
+async function find() {
+    const actions = await db("actions");
+    return actions;
+}
+
+async function findById(id) {
+    const action = await db("actions")
+        .where({ "actions.id": id })
+        .first();
+    return action;
+}
+
+async function create(item) {
+    const [id] = await db("actions").insert(item);
+    if(id) {
+        const action = await findById(id);
+        return action;
+    }
+}
+
+async function remove(id) {
+    const action = await findById(id);
+    if(action) {
+        const deleted = await db("actions")
+            .where({ id })
+            .del();
+        if(deleted) {
+            return action;
+        }  
+    }
+}
+
+async function update(item, id) {
+    const editedAction = await db("actions")
+        .where({ id })
+        .update(item);
+    if (editedAction) {
+        const action = await findById(id);
+        return action;
+    }
+}
--- data/helpers/projects-model.js
+++ data/helpers/projects-model.js
@@ -7,3 +7,51 @@ module.exports = {
     remove,
     update
 };
+
+async function find() {
+    const projects = await db("projects");
+    return projects;
+}
+
+async function findById(id) {
+    const project = await db("projects")
+        .select({
+            name: "projects.name",
+            id: "project.id",
+            actions: "actions.name"
+        })
+        .innerJoin("actions", "actions.project_id", "projects.id")
+        .where({ "projects.id": id })
+        .first();
+    return project;
+}
+
+async function create(item) {
+    const [id] = await db("projects").insert(item);
+    if(id) {
+        const project = await findById(id);
+        return project;
+    }
+}
+
+async function remove(id) {
+    const project = await findById(id);
+    if (project) {
+        const deleted = await db("projects")
+            .where({ id })
+            .del();
+        if (deleted) {
+            return project;
+        }
+    }
+}
+
+async function update(item, id) {
+    const editedproject = await db("projects")
+        .where({ id })
+        .update(item);
+    if (editedproject) {
+        const project = await findById(id);
+        return project;
+    }
+}
