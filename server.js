fd830c81199b7b72f04ed95fd9afcdb0f7a8341c
harjothkhara <harjoth.khara@gmail.com>
2019-06-07 08:43:59 -0700

    server initialized

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+//middleware
+server.use(helmet());
+server.use(express.json());
+
+
+
+
+
+module.exports = server;
